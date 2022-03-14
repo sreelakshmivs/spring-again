4d21c58d1ec44035067d7b1ac70128b9788b68ec
sreelakshmivs <vssreelakshmi24@gmail.com>
2022-03-14 14:58:25 +0100

    Spring based test completed

--- src/test/java/spring/again/basics/springagain/BinarySearchImplTest.java
+++ src/test/java/spring/again/basics/springagain/BinarySearchImplTest.java
@@ -1,17 +1,25 @@
 package spring.again.basics.springagain;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import spring.again.basics.springagain.springin5steps.SpringAgainBasicApplication;
+import spring.again.basics.springagain.springin5steps.basics.BinarySearchImpl;
 
 import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = SpringAgainBasicApplication.class)
+@SpringBootTest(classes = SpringAgainBasicApplication.class) //If you are not using springboot use: @ContextConfiguration(classes = classname.class) and also load the context using @RunWith(SpringRunner.class)
 class BinarySearchImplTest {
+
+	@Autowired
+	BinarySearchImpl binarySearch;
 
 	@Test
 	public void testBasicScenario() {
-		fail("Not yet implemented");
+		int result = binarySearch.binarySearch(new int[]{}, 5);
+		System.out.println(result);
+		assertEquals(3, result);
 	}
 
 }
